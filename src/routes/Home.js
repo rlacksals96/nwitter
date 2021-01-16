972a918ccd15305a9fe87996f4664c7e982d47b9
kimchanmin <rlacksals96@gmail.com>
2021-01-17 00:21:55 +0900

    modified

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Home=()=>{
-    return(<span>Home</span>)
+    return(
+        <div>
+            <form>
+                <input type="text" placeholder="what's on your mind" maxLength={120}/>
+                <input type="submit" value="Nweet"/>
+            </form>
+        </div>
+    )
 }
 
 export default Home;
